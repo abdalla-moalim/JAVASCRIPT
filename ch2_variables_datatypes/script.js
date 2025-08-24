f4b962a6e437a53fbde2af2a9ca85c079b9081fa
Abdalla <admali098@gmail.com>
2025-08-24 18:06:09 +0100

    this is my javascript course from the software development bootcamp

---
+++ ch2_variables_datatypes/script.js
@@ -0,0 +1,18 @@
+var firstName = "Abdalla";
+var lastName = "Moalim";
+var age = 25;
+var isStudent = true;
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+console.log(isStudent);
+
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+console.log(isStudent);
+
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+console.log(isStudent);
