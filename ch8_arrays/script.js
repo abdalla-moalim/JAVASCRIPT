9670c808d6cddcd38c6435cb68c8a21fec781cd7
Abdalla <admali098@gmail.com>
2025-09-14 17:06:50 +0100

    I have added arrays and loops

---
+++ ch8_arrays/script.js
@@ -0,0 +1,18 @@
+let firstNames = ["Ali", "Ahmed", "Yusuf", "Abdalla", "Zakaria"];
+// firstNames = new Array("Ali", "Ahmed", "Yusuf");
+// firstNames.push("Abdalla");
+// console.log(firstNames[2]);
+// firstNames.pop()
+// firstNames.unshift("Haji");
+
+// firstNames.shift();
+// firstNames[0] = "Abdalla";
+
+let shortlist = firstNames.slice(0, 3);
+console.log(shortlist);
+console.log(firstNames);
+// console.log(firstNames.length);
+
+// let listOfScores = []
+// listOfScores.push(5)
+// console.log(listOfScores);
---
+++ ch9_loops/for.js
@@ -0,0 +1,18 @@
+// for(let count = 0; count <= 50; count++){
+//     console.log(count + " " +"Abdalla");
+// }
+
+// let count = 0;
+// while (count <= 50) {
+//   console.log(count + " " + "Abdalla");
+//   count++;
+// }
+
+for (let i = 0; i <= 10; i++) {
+  if (i == 5) {
+    //   break
+    continue;
+  }
+
+  console.log(i + " " + "Abdalla");
+}
